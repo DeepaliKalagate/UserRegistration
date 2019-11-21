2c5f2bb5296acc1d7db6512ecc99871fb82b9247
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-21 14:10:47 +0530

    Add : Function for Validation of User First Name Using Regex

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,2 +1,17 @@
 #!/bin/bash 
 echo "Welcome to User Ragistration Using Regex"
+
+#function for User First Name Validation
+function userFirstName()
+{
+	echo "Enter First Name of User : "
+	read firstName
+
+	firstNamePattern=[A-Z][a-z]+{3,20}
+	if [[ $firstName =~ $firstNamePattern ]];
+	then
+        	echo "Welcome $firstName"
+	else
+        	echo "Invalid User Name : $firstName"
+	fi
+}
